48a795d954d791b455cb2923739b277698b22e6d
Valentine L <anix.mig29@gmail.com>
2020-10-01 16:44:59 +0300

    Scoped work with queries' use cases

--- app/src/main/java/com/epam/valkaryne/openmovieapp/vm/QueryHistoryViewModel.kt
+++ app/src/main/java/com/epam/valkaryne/openmovieapp/vm/QueryHistoryViewModel.kt
@@ -3,10 +3,12 @@ package com.epam.valkaryne.openmovieapp.vm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.epam.valkaryne.openmovieapp.common.QueryModel
 import com.epam.valkaryne.openmovieapp.domain.usecase.ClearQueryHistoryUseCase
 import com.epam.valkaryne.openmovieapp.domain.usecase.GetQueryHistoryUseCase
 import com.epam.valkaryne.openmovieapp.domain.usecase.SaveQueryHistoryUseCase
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel that works with storage of queries' history
@@ -30,19 +32,25 @@ class QueryHistoryViewModel(
      * @param query a query to save
      */
     fun saveQueryHistory(query: QueryModel) {
-        saveQueryHistoryUseCase.executeUseCase(query)
+        viewModelScope.launch {
+            saveQueryHistoryUseCase.executeUseCase(query)
+        }
     }
 
     /**
      * Clears the history of queries
      */
     fun clearQueryHistory() {
-        clearQueryHistoryUseCase.executeUseCase()
+        viewModelScope.launch {
+            clearQueryHistoryUseCase.executeUseCase()
+        }
         updateQueryHistory()
     }
 
     private fun updateQueryHistory() {
-        val history = getQueryHistoryUseCase.executeUseCase()
-        _queryHistory.value = history
+        viewModelScope.launch {
+            val history = getQueryHistoryUseCase.executeUseCase()
+            _queryHistory.value = history
+        }
     }
 }
